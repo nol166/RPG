5bfcf230c68c13083bb3816272c1e89197f47731
John McCambridge <jmccamb@gmail.com>
2020-07-17 13:46:18 -0500

    add use potion method

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -65,3 +65,33 @@ test('subtracts from player health', () => {
     player.reduceHealth(99999)
     expect(player.health).toBe(0)
 })
+
+// veryify attack value
+test("gets player's attack value", () => {
+    const player = new Player('Dave')
+    player.strength = 10
+
+    expect(player.getAttackValue()).toBeGreaterThanOrEqual(5)
+    expect(player.getAttackValue()).toBeLessThanOrEqual(15)
+})
+
+/* verify potion addition */
+test('adds a potion to the inventory', () => {
+    const player = new Player('Dave')
+    const oldCount = player.inventory.length
+
+    player.addPotion(new Potion())
+
+    expect(player.inventory.length).toBeGreaterThan(oldCount)
+})
+
+// use potion
+test('uses a potion from inventory', () => {
+    const player = new Player('Dave')
+    player.inventory = [new Potion(), new Potion(), new Potion()]
+    const oldCount = player.inventory.length
+
+    player.usePotion(1)
+
+    expect(player.inventory.length).toBeLessThan(oldCount)
+})
